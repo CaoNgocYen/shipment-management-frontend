3089842297cfb9dcd0144866fe114f6c04b5241a
CaoNgocYen <s0577979@htw-berlin.de>
2022-06-06 11:36:07 +0200

    add Navbar components

---
+++ src/views/ShipmentView.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>This is shipment page</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Shipment'
+}
+</script>
+
+<style scoped>
+
+</style>
